aaf222074c399195d02602552f356fedd45a00d6
Eden Nana Obeng Kyei <eobengky@caltech.edu>
2025-06-02 21:28:51 -0700

    scene score added to json

--- 3D_craft/assets/blender/script.py
+++ 3D_craft/assets/blender/script.py
@@ -38,78 +38,78 @@ floor.scale = (2.0, 1.0, 0.05)
 # -------------------------------------------------- Walls
 bpy.ops.mesh.primitive_cube_add(location=(2.0, 0, 1.5))
 wall = bpy.context.active_object
-wall.name = 'wall1'
+wall.name = 'wall_1'
 wall.scale = (2.0, 0.1, 1.5)
 
 bpy.ops.mesh.primitive_cube_add(location=(4, 1.0, 1.5))
 wall = bpy.context.active_object
-wall.name = 'wall2'
+wall.name = 'wall_2'
 wall.scale = (0.1, 1.0, 1.5)
 
 bpy.ops.mesh.primitive_cube_add(location=(2.0, 2, 1.5))
 wall = bpy.context.active_object
-wall.name = 'wall3'
+wall.name = 'wall_3'
 wall.scale = (2.0, 0.1, 1.5)
 
 bpy.ops.mesh.primitive_cube_add(location=(0, 1.0, 1.5))
 wall = bpy.context.active_object
-wall.name = 'wall4'
+wall.name = 'wall_4'
 wall.scale = (0.1, 1.0, 1.5)
 
 # -------------------------------------------------- Furniture
-# bed  (bed)
-bpy.ops.mesh.primitive_cube_add(location=(0.90, 0.50, 0.30))
+# furn_bed  (bed)
+bpy.ops.mesh.primitive_cube_add(location=(1.00, 0.60, 0.30))
 obj = bpy.context.active_object
-obj.name = 'bed'
-obj.scale = (0.95, 0.45, 0.30)
+obj.name = 'furn_bed'
+obj.scale = (1.00, 0.50, 0.30)
 obj.rotation_euler = (0, 0, 0)
 
-# desk  (desk)
-bpy.ops.mesh.primitive_cube_add(location=(3.00, 1.30, 0.38))
+# furn_desk  (desk)
+bpy.ops.mesh.primitive_cube_add(location=(3.00, 0.75, 0.38))
 obj = bpy.context.active_object
-obj.name = 'desk'
-obj.scale = (0.60, 0.30, 0.38)
-obj.rotation_euler = (0, 0, 0)
+obj.name = 'furn_desk'
+obj.scale = (0.50, 0.30, 0.38)
+obj.rotation_euler = (0, 90, 0)
 
-# lamp  (lamp)
-bpy.ops.mesh.primitive_cylinder_add(location=(3.60, 1.60, 0.25))
+# furn_lamp  (lamp)
+bpy.ops.mesh.primitive_cylinder_add(location=(3.20, 0.50, 0.25))
 obj = bpy.context.active_object
-obj.name = 'lamp'
+obj.name = 'furn_lamp'
 obj.scale = (0.10, 0.10, 0.25)
 obj.rotation_euler = (0, 0, 0)
 
 # -------------------------------------------------- Materials
-mat = bpy.data.materials.new(name='mat_wall')
+mat = bpy.data.materials.new(name='wall_mat')
 mat.use_nodes = True
 bsdf = mat.node_tree.nodes['Principled BSDF']
-bsdf.inputs['Base Color'].default_value = (0.004, 0.004, 0.004, 1.0)
-bsdf.inputs['Roughness'].default_value = 0.8
+bsdf.inputs['Base Color'].default_value = (1.000, 1.000, 1.000, 1.0)
+bsdf.inputs['Roughness'].default_value = 0.5
 
-mat = bpy.data.materials.new(name='mat_floor')
+mat = bpy.data.materials.new(name='floor_mat')
 mat.use_nodes = True
 bsdf = mat.node_tree.nodes['Principled BSDF']
-bsdf.inputs['Base Color'].default_value = (0.002, 0.002, 0.002, 1.0)
-bsdf.inputs['Roughness'].default_value = 0.8
+bsdf.inputs['Base Color'].default_value = (0.490, 0.490, 0.490, 1.0)
+bsdf.inputs['Roughness'].default_value = 0.5
 
 floor = bpy.data.objects.get('Floor')
-if floor and 'mat_floor' in bpy.data.materials:
-    floor.data.materials.append(bpy.data.materials['mat_floor'])
+if floor and 'floor_mat' in bpy.data.materials:
+    floor.data.materials.append(bpy.data.materials['floor_mat'])
 
-w = bpy.data.objects.get('wall1')
-if w and 'mat_wall' in bpy.data.materials:
-    w.data.materials.append(bpy.data.materials['mat_wall'])
+w = bpy.data.objects.get('wall_1')
+if w and 'wall_mat' in bpy.data.materials:
+    w.data.materials.append(bpy.data.materials['wall_mat'])
 
-w = bpy.data.objects.get('wall2')
-if w and 'mat_wall' in bpy.data.materials:
-    w.data.materials.append(bpy.data.materials['mat_wall'])
+w = bpy.data.objects.get('wall_2')
+if w and 'wall_mat' in bpy.data.materials:
+    w.data.materials.append(bpy.data.materials['wall_mat'])
 
-w = bpy.data.objects.get('wall3')
-if w and 'mat_wall' in bpy.data.materials:
-    w.data.materials.append(bpy.data.materials['mat_wall'])
+w = bpy.data.objects.get('wall_3')
+if w and 'wall_mat' in bpy.data.materials:
+    w.data.materials.append(bpy.data.materials['wall_mat'])
 
-w = bpy.data.objects.get('wall4')
-if w and 'mat_wall' in bpy.data.materials:
-    w.data.materials.append(bpy.data.materials['mat_wall'])
+w = bpy.data.objects.get('wall_4')
+if w and 'wall_mat' in bpy.data.materials:
+    w.data.materials.append(bpy.data.materials['wall_mat'])
 
 # -------------------------------------------------- Save & Render
 import os
