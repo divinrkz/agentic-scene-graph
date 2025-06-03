731771af633a394328c74f409bb545efa75601fb
Eden Nana Obeng Kyei <eobengky@caltech.edu>
2025-06-02 20:09:08 -0700

    added prompts for single relations questions

--- 3D_craft/assets/blender/script.py
+++ 3D_craft/assets/blender/script.py
@@ -41,75 +41,75 @@ wall = bpy.context.active_object
 wall.name = 'wall1'
 wall.scale = (2.0, 0.1, 1.5)
 
-bpy.ops.mesh.primitive_cube_add(location=(4, 0.0, 1.5))
+bpy.ops.mesh.primitive_cube_add(location=(4, 1.0, 1.5))
 wall = bpy.context.active_object
 wall.name = 'wall2'
-wall.scale = (0.1, 0.0, 1.5)
+wall.scale = (0.1, 1.0, 1.5)
 
-bpy.ops.mesh.primitive_cube_add(location=(2.0, 0, 1.5))
+bpy.ops.mesh.primitive_cube_add(location=(2.0, 2, 1.5))
 wall = bpy.context.active_object
 wall.name = 'wall3'
 wall.scale = (2.0, 0.1, 1.5)
 
-bpy.ops.mesh.primitive_cube_add(location=(0, 0.0, 1.5))
+bpy.ops.mesh.primitive_cube_add(location=(0, 1.0, 1.5))
 wall = bpy.context.active_object
 wall.name = 'wall4'
-wall.scale = (0.1, 0.0, 1.5)
+wall.scale = (0.1, 1.0, 1.5)
 
 # -------------------------------------------------- Furniture
 # bed  (bed)
-bpy.ops.mesh.primitive_cube_add(location=(1.00, 0.00, 0.25))
+bpy.ops.mesh.primitive_cube_add(location=(0.90, 0.50, 0.30))
 obj = bpy.context.active_object
 obj.name = 'bed'
-obj.scale = (1.00, 0.50, 0.25)
+obj.scale = (0.95, 0.45, 0.30)
 obj.rotation_euler = (0, 0, 0)
 
 # desk  (desk)
-bpy.ops.mesh.primitive_cube_add(location=(2.50, 0.00, 0.35))
+bpy.ops.mesh.primitive_cube_add(location=(3.00, 1.30, 0.38))
 obj = bpy.context.active_object
 obj.name = 'desk'
-obj.scale = (0.75, 0.30, 0.35)
+obj.scale = (0.60, 0.30, 0.38)
 obj.rotation_euler = (0, 0, 0)
 
 # lamp  (lamp)
-bpy.ops.mesh.primitive_cylinder_add(location=(2.80, 0.00, 0.25))
+bpy.ops.mesh.primitive_cylinder_add(location=(3.60, 1.60, 0.25))
 obj = bpy.context.active_object
 obj.name = 'lamp'
 obj.scale = (0.10, 0.10, 0.25)
 obj.rotation_euler = (0, 0, 0)
 
 # -------------------------------------------------- Materials
-mat = bpy.data.materials.new(name='plaster_wall')
+mat = bpy.data.materials.new(name='mat_wall')
 mat.use_nodes = True
 bsdf = mat.node_tree.nodes['Principled BSDF']
-bsdf.inputs['Base Color'].default_value = (1.000, 1.000, 1.000, 1.0)
-bsdf.inputs['Roughness'].default_value = 0.6
+bsdf.inputs['Base Color'].default_value = (0.004, 0.004, 0.004, 1.0)
+bsdf.inputs['Roughness'].default_value = 0.8
 
-mat = bpy.data.materials.new(name='carpet')
+mat = bpy.data.materials.new(name='mat_floor')
 mat.use_nodes = True
 bsdf = mat.node_tree.nodes['Principled BSDF']
-bsdf.inputs['Base Color'].default_value = (0.471, 0.471, 0.471, 1.0)
+bsdf.inputs['Base Color'].default_value = (0.002, 0.002, 0.002, 1.0)
 bsdf.inputs['Roughness'].default_value = 0.8
 
 floor = bpy.data.objects.get('Floor')
-if floor and 'carpet' in bpy.data.materials:
-    floor.data.materials.append(bpy.data.materials['carpet'])
+if floor and 'mat_floor' in bpy.data.materials:
+    floor.data.materials.append(bpy.data.materials['mat_floor'])
 
 w = bpy.data.objects.get('wall1')
-if w and 'plaster_wall' in bpy.data.materials:
-    w.data.materials.append(bpy.data.materials['plaster_wall'])
+if w and 'mat_wall' in bpy.data.materials:
+    w.data.materials.append(bpy.data.materials['mat_wall'])
 
 w = bpy.data.objects.get('wall2')
-if w and 'plaster_wall' in bpy.data.materials:
-    w.data.materials.append(bpy.data.materials['plaster_wall'])
+if w and 'mat_wall' in bpy.data.materials:
+    w.data.materials.append(bpy.data.materials['mat_wall'])
 
 w = bpy.data.objects.get('wall3')
-if w and 'plaster_wall' in bpy.data.materials:
-    w.data.materials.append(bpy.data.materials['plaster_wall'])
+if w and 'mat_wall' in bpy.data.materials:
+    w.data.materials.append(bpy.data.materials['mat_wall'])
 
 w = bpy.data.objects.get('wall4')
-if w and 'plaster_wall' in bpy.data.materials:
-    w.data.materials.append(bpy.data.materials['plaster_wall'])
+if w and 'mat_wall' in bpy.data.materials:
+    w.data.materials.append(bpy.data.materials['mat_wall'])
 
 # -------------------------------------------------- Save & Render
 import os
